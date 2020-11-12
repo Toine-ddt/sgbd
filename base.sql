88ce4792a578cf4d32cc381086d158da8c84f5ee
BOISSEAU Alexis <Alexis.Boisseau@bordeaux-inp.fr>
2020-11-12 15:49:23 +0100

    ajout des cles secondaires et implémentation du modele relationnel

--- base.sql
+++ base.sql
@@ -1,9 +1,26 @@
 -- ============================================================
 --   Nom de la base   :  Fédération sportive de Hand-ball                                
---   Nom de SGBD      :                      
+--   Nom de SGBD      :  ORACLE Version 7.0                    
 --   Date de creation :  10/11/20  09:08                       
 -- ============================================================
 
+drop table JOUEUR cascade constraints;
+
+drop table EQUIPE cascade constraints;
+
+drop table CLUB cascade constraints;
+
+drop table MEMBRE cascade constraints;
+
+drop table ENTRAINEUR cascade constraints;
+
+drop table RENCONTRE cascade constraints;
+
+drop table Entrainer cascade constraints;
+
+drop table Participer cascade constraints;
+
+drop table Effectuer cascade constraints;
 
 -- ============================================================
 --   Table : JOUEUR                                            
@@ -28,7 +45,7 @@ create table EQUIPE
     NUMERO_EQUIPE                   NUMBER(10)              not null,
     NOM_CATEGORIE                   CHAR(20)                not null,
     NUMERO_CATEGORIE                NUMBER(10)                      ,
-    constraint pk_joueur primary key (NUMERO_EQUIPE)
+    constraint pk_equipe primary key (NUMERO_EQUIPE)
 );
 
 
@@ -38,8 +55,8 @@ create table EQUIPE
 
 create table CLUB
 (
-    NUMERO_CLUB                     NUMBER(10)              not null,                 
-    constraint pk_joueur primary key (NUMERO_CLUB)
+    NUMERO_CLUB                     NUMBER(10)              not null,                              
+    constraint pk_club primary key (NUMERO_CLUB)
 );
 
 
@@ -55,8 +72,9 @@ create table MEMBRE
     NUMERO_TEL_MEMBRE               CHAR(20)                        ,
     ADRESSE_MAIL_MEMBRE             CHAR(20)                        ,
     DATE_DE_NAISSANCE_MEMBRE        DATE                            ,
-    ADRESSE_MEMBRE                  CHAR(20)                        ,                 
-    constraint pk_joueur primary key (NUMERO_MEMBRE)
+    ADRESSE_MEMBRE                  CHAR(20)                        , 
+    POSTE                           CHAR(20)                        ,                 
+    constraint pk_membre primary key (NUMERO_MEMBRE)
 );
 
 
@@ -70,7 +88,7 @@ create table ENTRAINEUR
     NOM_ENTRAINEUR                   CHAR(20)               not null,
     PRENOM_ENTRAINEUR                CHAR(20)                       ,
     DATE_ENTREE_ENTRAINEUR           DATE                           ,
-    constraint pk_joueur primary key (NUMERO_ENTRAINEUR)
+    constraint pk_entraineur primary key (NUMERO_ENTRAINEUR)
 );
 
 
@@ -83,6 +101,80 @@ create table RENCONTRE
     NUMERO_RENCONTRE                NUMBER(10)             not null,
     DATE_RENCONTRE                  DATE                           ,
     SCORE                           NUMBER(10)                     ,
-    constraint pk_joueur primary key (NUMERO_RENCONTRE)
+    constraint pk_rencontre primary key (NUMERO_RENCONTRE)
 );
+
+-- ============================================================
+--   Table : Entrainer                                            
+-- ============================================================
+
+create table Entrainer
+(
+    NUMERO_EQUIPE                   NUMBER(10)              not null,
+    NUMERO_ENTRAINEUR               NUMBER(10)              not null,
+    DATE_ENTREE_EQUIPE              DATE                            ,
+    constraint pk_entrainer primary key (NUMERO_EQUIPE, NUMERO_ENTRAINEUR)
+);
+
+-- ============================================================
+--   Table : Participer                                            
+-- ============================================================
+
+create table Participer
+(
+    NUMERO_EQUIPE                   NUMBER(10)              not null,
+    NUMERO_RENCONTRE                NUMBER(10)              not null,                         ,
+    constraint pk_participer primary key (NUMERO_EQUIPE, NUMERO_RENCONTRE)
+);
+
+-- ============================================================
+--   Table : Effectuer                                            
+-- ============================================================
+
+create table Effectuer
+(
+    NUMERO_LICENCE                  NUMBER(10)              not null,
+    NUMERO_RENCONTRE                NUMBER(10)              not null, 
+    NOMBRE_POINTS_MARQUES           NUMBER(10)                      ,
+    NOMBRE_FAUTES                   NUMBER(10)                      ,
+    constraint pk_effectuer primary key (NUMERO_LICENCE, NUMERO_RENCONTRE)
+);
+
+alter table MEMBRE 
+    add constraint fk_NUMERO_MEMBRE_CLUB foreign key (NUMERO_CLUB) 
+    references  CLUB (NUMERO_CLUB);
+
+alter table CLUB 
+    add constraint fk_NUMERO_CLUB_LICENCE foreign key (NUMERO_LICENCE) 
+    references  JOUEUR (NUMERO_LICENCE);
+
+alter table JOUEUR 
+    add constraint fk_NUMERO_JOUEUR_EQUIPE foreign key (NUMERO_EQUIPE) 
+    references  EQUIPE (NUMERO_EQUIPE);
+
+alter table EQUIPE 
+    add constraint fk_NUMERO_EQUIPE_CLUB foreign key (NUMERO_CLUB) 
+    references  CLUB (NUMERO_CLUB);
+
+alter table Entrainer 
+    add constraint fk_NUMERO_Entrainer_EQUIPE foreign key (NUMERO_EQUIPE) 
+    references  EQUIPE (NUMERO_EQUIPE);
+alter table Entrainer 
+    add constraint fk_NUMERO_Entrainer_ENTRAINEUR foreign key (NUMERO_ENTRAINEUR) 
+    references  ENTRAINEUR (NUMERO_ENTRAINEUR);
+
+alter table Participer 
+    add constraint fk_NUMERO_Participer_EQUIPE foreign key (NUMERO_EQUIPE) 
+    references  EQUIPE (NUMERO_EQUIPE);
+alter table Participer 
+    add constraint fk_NUMERO_Participer_RENCONTRE foreign key (NUMERO_RENCONTRE) 
+    references  RENCONTRE (NUMERO_RENCONTRE);
+
+alter table Effectuer 
+    add constraint fk_NUMERO_Effectuer_LICENCE foreign key (NUMERO_LICENCE) 
+    references  JOUEUR (NUMERO_LICENCE);
+alter table Effectuer 
+    add constraint fk_NUMERO_Effectuer_RENCONTRE foreign key (NUMERO_RENCONTRE) 
+    references  RENCONTRE (NUMERO_RENCONTRE);
+
 
